2002d621b8cd7892f3d883715bb451db9614c62b
Artem Arhipov <artemiy3893@gmail.com>
2020-06-20 08:13:56 +0300

    starting field

--- Source.cpp
+++ Source.cpp
@@ -24,8 +24,20 @@ void startingField(Checker** field) {
 	for (int i = 0; i < SIZE; i++) {
 		for (int j = 0; j < SIZE; j++) {
 			if (((i + j) % 2 == 0 && (i < 3)) || ((i + j) % 2 != 0 && (i >= 5))) {
+				field[i][j].isEmpty = true;
 				if (i < 3) {
-					
+					if ((i + j) % 2 == 0) {
+						field[i][j].space = CHECKER;
+						field[i][j].isWhite = false;
+						field[i][j].isEmpty = false;
+					}
+				}
+				if (i >= 5) {
+					if ((i + j) % 2 != 0) {
+						field[i][j].space = CHECKER;
+						field[i][j].isWhite = true;
+						field[i][j].isEmpty = false;
+					}
 				}
 			}
 		}
